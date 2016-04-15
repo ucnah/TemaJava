5d763a29055830bb5b7f05afe0e9cbdc08a46f61
ucnah <andrei_hancu2002@yahoo.com>
2016-04-15 13:16:30 +0300

    Project Created!!
    Am creat proiectul folosind maven cu dependinta la jpa ...Iti pun
    structura sa o ai si tu

---
+++ PaymentSystem/src/main/java/com/scaietii/PaymentSystem/App.java
@@ -0,0 +1,14 @@
+package com.scaietii.PaymentSystem;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
